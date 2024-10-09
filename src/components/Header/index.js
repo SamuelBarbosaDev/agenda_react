2ffe99fe36683797a4dd6e2edd21b5f6175a074a
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-10-09 17:49:13 -0300

    feat: adicionando `Logout`

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { HeaderContent } from './styled';
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import history from '../../services/history';
+import { useSelector, useDispatch } from 'react-redux';
+import * as actions from '../../store/modules/auth/actions';
+import {
+    FaHome,
+    FaSignInAlt,
+    FaUserAlt,
+    FaCircle,
+    FaPowerOff
+} from 'react-icons/fa';
 
 export default function Header(){
+    const dispatch = useDispatch();
+    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
+    const handleLogout = e => {
+        e.preventDefault();
+        dispatch(actions.loginFailure());
+        history.push('/');
+    }
     return (
     <HeaderContent>
         <section className="header-content">
@@ -14,9 +31,17 @@ export default function Header(){
                 <Link to="/register">
                     <FaUserAlt size={24}/>
                 </Link>
-                <Link to="/login">
-                    <FaSignInAlt size={24}/>
-                </Link>
+                { isLoggedIn ? (
+                    <Link onClick={handleLogout} to="/logout">
+                        <FaPowerOff size={24}/>
+                    </Link>
+                ) : (
+                    <Link to="/login">
+                        <FaSignInAlt size={24}/>
+                    </Link>
+                )}
+
+                {isLoggedIn && <FaCircle size={24} color='#66ff33'/>}
             </nav>
         </section>
     </HeaderContent>
