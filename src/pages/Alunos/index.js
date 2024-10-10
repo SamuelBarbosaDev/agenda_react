7c4b6bd62d4692341ebf67228e763ef8920be626
SamuelBarbosaDev <samueloficial@protonmail.com>
2024-10-10 16:23:39 -0300

    feat: Adicionando funcionalidade de  `excluir alunos`

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,10 +1,16 @@
 import { get } from 'lodash'
+import { toast } from 'react-toastify';
 import axios from '../../services/axios';
 import { Main, ListaAlunos } from './styled';
 import React, { useEffect, useState } from 'react';
-import { FaEdit, FaWindowClose, FaUserCircle } from 'react-icons/fa'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Loading from '../../components/Loading';
+import {
+    FaEdit,
+    FaWindowClose,
+    FaUserCircle,
+    FaExclamation
+} from 'react-icons/fa'
 
 
 export default function Alunos(){
@@ -18,13 +24,44 @@ export default function Alunos(){
         }
         getData();
     }, []);
+
+    const handleDeleteAsk = e => {
+        e.preventDefault();
+        const exclamation = e.currentTarget.nextSibling;
+        exclamation.setAttribute('display', 'block');
+        e.currentTarget.remove();
+    }
+
+    const handleDelete = async(e, id, index) => {
+        e.persist();
+
+        try{
+            setIsLoading(true);
+            await axios.delete(`/alunos/${id}`);
+            const novosAlunos = [...alunos];
+            novosAlunos.splice(index, 1);
+            setAlunos(novosAlunos);
+            setIsLoading(false);
+        }
+        catch(err){
+            const status = get(err, 'response.status', 0)
+
+            if(status === 401){
+                toast.error('Você precisa fazer login.');
+            }
+            else{
+                toast.error('Ocorreu um erro ao excluir aluno.')
+            }
+            setIsLoading(false)
+        }
+    }
     return (
     <Main>
         <Loading isLoading={isLoading} />
         <section className="main-content">
         <h1>Alunos</h1>
         <ListaAlunos>
-            {alunos.map(aluno => (
+            {alunos.map((aluno, index) => (
                 <li key={String(aluno.id)}>
                     <section className='student-content'>
                         <div className='profile-picture'>
@@ -47,9 +84,15 @@ export default function Alunos(){
                         <Link to={`/aluno/${aluno.id}/edit`}>
                             <FaEdit className='student-button-edit' />
                         </Link>
-                        <Link to={`/aluno/${aluno.id}/delete`}>
+                        <Link onClick={handleDeleteAsk} to={`/aluno/${aluno.id}/delete`}>
                             <FaWindowClose className='student-button-delete' />
                         </Link>
+                        <FaExclamation
+                            size={16}
+                            display="none"
+                            cursor="pointer"
+                            onClick={e => handleDelete(e, aluno.id, index)}
+                        />
                     </div>
                 </li>
             ))}
